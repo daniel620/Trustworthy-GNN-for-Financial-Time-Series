c56f0959ed4ad5aa8f43c8f37cd15db7ee98bdab
Hugo Zhou <hz475@r903u37n01.grace.ycrc.yale.edu>
2024-05-02 15:52:27 -0400

    explanation

---
+++ modelling/grad_explain.py
@@ -0,0 +1,21 @@
+import torch
+
+def integrated_gradients(input, edge_index, baseline, model, steps=50, batch_size_index=0, node_index=0):
+    input = input.clone().detach().requires_grad_(True)
+    baseline = baseline.clone().detach()
+
+    interpolated = [baseline + (step / steps) * (input - baseline) for step in range(0, steps + 1)]
+
+    grads = []
+    for i, inp in enumerate(interpolated):
+        if inp.grad is not None:
+            inp.grad.zero_()
+        inp.requires_grad_(True)
+        with torch.autograd.set_grad_enabled(True):
+            output = model(inp, edge_index)
+            target_score = output[batch_size_index, node_index] # target score at the node of interest
+            grads.append(torch.autograd.grad(target_score, inp)[0][batch_size_index])
+
+    avg_grads = torch.stack(grads).mean(dim=0)
+    integrated_grad = (input - baseline)[batch_size_index, node_index] * avg_grads  # element-wise multiplication
+    return integrated_grad
